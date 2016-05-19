7dc13437bcf522a118bcda2c5d0d17e5643607bb
AdamVenis <adamvenis@gmail.com>
2016-05-19 19:00:26 -0400

    switched pieces from classes to functions, cleaned up iterative minimax to be more uniform

--- README.md
+++ README.md
@@ -5,3 +5,6 @@ Usage:
 Install PyInstaller for Python 3.x
 Run "pyinstaller --onefile metagrobolizer.py" to create the executable
 Load the executable as an engine into your favourite UCI-compatible chess GUI, such as Arena or Tarrasch.
+
+Changelog:
+2016-05-18: recursive to iterative minimax: 44s to 37s for 3 moves at depth 3.
--- metagrobolizer.py
+++ metagrobolizer.py
@@ -3,6 +3,7 @@
 #   remove recursion
 #   modify board instance instead of create new instance (test this)
 #   use timer module to evaluate for a certain time period
+#   make the agent stop playing with its food (most direct checkmate)
 ##
 
 import cmd
@@ -45,44 +46,38 @@ class MinMaxAgent():
         self.depth = depth
 
     def move(self, game):
-        running_results = [(-10**6 * (-1)**i, None) for i in range(self.depth)]
-        node_stack = [[], [(simulate(move), move) for move in game.moves()]]
-        stack_depth = 0
-        while node_stack:
+        running_results = [(-10**6 * (-1)**i, None) for i in range(self.depth + 1)]
+        nodes = [[(simulate(move), move) for move in game.moves()]]
+        while nodes[0]:
             # expand(nodes)
-            while len(node_stack) <= self.depth:
-                node_stack.append([(simulate(move), node_stack[-1][-1][1])
-                    for move in node_stack[-1][-1][0].moves()])
-                node_stack[-2].pop()
+            while len(nodes) < self.depth:
+                nodes.append([(simulate(move), nodes[-1][-1][1])
+                    for move in nodes[-1][-1][0].moves()])
+                nodes[-2].pop()
 
-            # aggregate(bottom_level)
-            m = max if self.depth % 2 == 1 else min
-            running_results[-1] = m((self.static_eval(g)*(-1)**game.turn, move)
-                                    for (g, move) in node_stack[-1])
-            node_stack[-1] = []
+            # evaluate deepest node
+            g, move = nodes[-1].pop()
+            running_results[-1] = (self.static_eval(g)*(-1)**game.turn, move)
 
-            # update(running_results)
-            i = self.depth - 1
-            while i > 0 and not node_stack[i + 1]:
-                m = max if i % 2 == 1 else min
-                running_results[i - 1] = m(running_results[i - 1], running_results[i])
-                running_results[i] = (-10**6 * (-1)**i, None)
-                del node_stack[i + 1]
-                i -= 1
-
-            if not node_stack[1]:
-                break
+            # update running results
+            for i in range(self.depth - 1, -1, -1):
+                m = max if i % 2 == 0 else min
+                running_results[i] = m(running_results[i], running_results[i + 1])
+                running_results[i + 1] = (10**6 * (-1)**i, None)
+                if nodes[i]:
+                    break
+                del nodes[i]
         return running_results[0][1]
 
-
     def static_eval(self, game):
-        return self.eval_player(game, WHITE) - self.eval_player(game, BLACK)
-
-    def eval_player(self, game, colour):
         if game.winner == WHITE:
             return 1000
         elif game.winner == BLACK:
             return -1000
+        else:
+            return self.eval_player(game, WHITE) - self.eval_player(game, BLACK)
+
+    def eval_player(self, game, colour):
         rtn = 0
         for piece in game.pieces:
             if colour == (ord(piece) > 97):
@@ -99,7 +94,7 @@ class MinMaxAgent():
                 elif piece in ['k', 'K']:
                     rtn += 1000 * len(game.pieces[piece])
                 for x, y, c in game.pieces[piece]:
-                    rtn += 0.25 * len(game.piece_objs(piece).moves(game, x, y, c))
+                    rtn += 0.1 * len(game.piece_objs(piece)(game, x, y, c))
         return rtn
 
 class Game():
@@ -142,24 +137,24 @@ class Game():
     def piece_objs(self, piece):
         piece = piece.lower()
         if piece == 'p':
-            return Pawn
+            return pawn
         elif piece == 'n':
-            return Knight
+            return knight
         elif piece == 'b':
-            return Bishop
+            return bishop
         elif piece == 'r':
-            return Rook
+            return rook
         elif piece == 'q':
-            return Queen
+            return queen
         elif piece == 'k':
-            return King
+            return king
 
     def moves(self):
         rtn = []
         for piece in self.pieces:
             for x, y, c in self.pieces[piece]:
                 if c == self.turn % 2:
-                    rtn.extend(self.piece_objs(piece).moves(self, x, y, c))
+                    rtn.extend(self.piece_objs(piece)(self, x, y, c))
         return rtn
 
     def square(self, x, y):
@@ -204,128 +199,85 @@ class Game():
     def __eq__(self, other):
         return str(self) == str(other)
 
+def pawn(game, x, y, colour):
+    rtn = []
+    direction = 1 if colour == WHITE else -1
+    if game.square(x, y + direction) == FREE:
+        rtn.append(Move(game, x, y, x, y + direction))
+        if (y + direction*6 in range(len(game.board)) and
+            game.square(x, y + 2 * direction) == FREE):
+            rtn.append(Move(game, x, y, x, y + 2 * direction))
+        elif y % 8 == (-1)**direction: # second last rank
+            pass # some promotion stuff
+    for capture_delta in [-1, 1]:
+        new_square = x + capture_delta, y + direction
+        if game.square(*new_square) == 1 - colour or new_square == game.en_passant:
+            rtn.append(Move(game, x, y, *new_square))
 
-class Piece():
-    def __init__(self, colour, posx, posy):
-        self.colour = colour
-        self.x = posx
-        self.y = posy
-
-
-class Pawn(Piece):
-    @staticmethod
-    def moves(game, x, y, colour):
-        rtn = []
-        direction = 1 if colour == WHITE else -1
-        if game.square(x, y + direction) == FREE:
-            rtn.append(Move(game, x, y, x, y + direction))
-            if (y + direction*6 in range(len(game.board)) and
-                game.square(x, y + 2 * direction) == FREE):
-                rtn.append(Move(game, x, y, x, y + 2 * direction))
-            elif y % 8 == (-1)**direction: # second last rank
-                pass # some promotion stuff
-        for capture_delta in [-1, 1]:
-            new_square = x + capture_delta, y + direction
-            if game.square(*new_square) == 1 - colour or new_square == game.en_passant:
-                rtn.append(Move(game, x, y, *new_square))
-
-        return rtn # still needs promotions
-
-    def __repr__(self):
-        return 'P' if self.colour == WHITE else 'p'
+    return rtn # still needs promotions
 
 
-class Knight(Piece):
-    @staticmethod
-    def moves(game, x, y, colour):
-        rtn = []
-        deltas = [(-2, -1), (-2, 1), (-1, -2), (-1, 2), (1, -2), (1, 2), (2, -1), (2, 1)]
-        for delta in deltas:
-            new_x = x + delta[0]
-            new_y = y + delta[1]
-            if game.square(new_x, new_y) in [FREE, 1 - colour]:
-                rtn.append(Move(game, x, y, new_x, new_y))
-        return rtn
+def knight(game, x, y, colour):
+    rtn = []
+    deltas = [(-2, -1), (-2, 1), (-1, -2), (-1, 2), (1, -2), (1, 2), (2, -1), (2, 1)]
+    for delta in deltas:
+        new_x = x + delta[0]
+        new_y = y + delta[1]
+        if game.square(new_x, new_y) in [FREE, 1 - colour]:
+            rtn.append(Move(game, x, y, new_x, new_y))
+    return rtn
 
-    def __repr__(self):
-        return 'N' if self.colour == WHITE else 'n'
+def bishop(game, x, y, colour):
+    rtn = []
+    deltas = [(-1, -1), (-1, 1), (1, -1), (1, 1)]
+    for delta in deltas:
+        new_x = x + delta[0]
+        new_y = y + delta[1]
+        while game.square(new_x, new_y) == FREE:
+            rtn.append(Move(game, x, y, new_x, new_y))
+            new_x += delta[0]
+            new_y += delta[1]
+        if game.square(new_x, new_y) == 1 - colour:
+            rtn.append(Move(game, x, y, new_x, new_y))
+    return rtn
 
+def rook(game, x, y, colour):
+    rtn = []
+    deltas = [(-1, 0), (0, -1), (0, 1), (1, 0)]
+    for delta in deltas:
+        new_x = x + delta[0]
+        new_y = y + delta[1]
+        while game.square(new_x, new_y) == FREE:
+            rtn.append(Move(game, x, y, new_x, new_y))
+            new_x += delta[0]
+            new_y += delta[1]
+        if game.square(new_x, new_y) == 1 - colour:
+            rtn.append(Move(game, x, y, new_x, new_y))
+    return rtn
 
-class Bishop(Piece):
-    @staticmethod
-    def moves(game, x, y, colour):
-        rtn = []
-        deltas = [(-1, -1), (-1, 1), (1, -1), (1, 1)]
-        for delta in deltas:
-            new_x = x + delta[0]
-            new_y = y + delta[1]
-            while game.square(new_x, new_y) == FREE:
-                rtn.append(Move(game, x, y, new_x, new_y))
-                new_x += delta[0]
-                new_y += delta[1]
-            if game.square(new_x, new_y) == 1 - colour:
-                rtn.append(Move(game, x, y, new_x, new_y))
-        return rtn
+def queen(game, x, y, colour):
+    rtn = []
+    deltas = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
+    for delta in deltas:
+        new_x = x + delta[0]
+        new_y = y + delta[1]
+        while game.square(new_x, new_y) == FREE:
+            rtn.append(Move(game, x, y, new_x, new_y))
+            new_x += delta[0]
+            new_y += delta[1]
+        if game.square(new_x, new_y) == 1 - colour:
+            rtn.append(Move(game, x, y, new_x, new_y))
+    return rtn
 
-
-    def __repr__(self):
-        return 'B' if self.colour == WHITE else 'b'
-
-
-class Rook(Piece):
-    @staticmethod
-    def moves(game, x, y, colour):
-        rtn = []
-        deltas = [(-1, 0), (0, -1), (0, 1), (1, 0)]
-        for delta in deltas:
-            new_x = x + delta[0]
-            new_y = y + delta[1]
-            while game.square(new_x, new_y) == FREE:
-                rtn.append(Move(game, x, y, new_x, new_y))
-                new_x += delta[0]
-                new_y += delta[1]
-            if game.square(new_x, new_y) == 1 - colour:
-                rtn.append(Move(game, x, y, new_x, new_y))
-        return rtn
-
-    def __repr__(self):
-        return 'R' if self.colour == WHITE else 'r'
-
-
-class Queen(Piece):
-    @staticmethod
-    def moves(game, x, y, colour):
-        rtn = []
-        deltas = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
-        for delta in deltas:
-            new_x = x + delta[0]
-            new_y = y + delta[1]
-            while game.square(new_x, new_y) == FREE:
-                rtn.append(Move(game, x, y, new_x, new_y))
-                new_x += delta[0]
-                new_y += delta[1]
-            if game.square(new_x, new_y) == 1 - colour:
-                rtn.append(Move(game, x, y, new_x, new_y))
-        return rtn
-
-    def __repr__(self):
-        return 'Q' if self.colour == WHITE else 'q'
-
-
-class King(Piece):
-    @staticmethod
-    def moves(game, x, y, colour):
-        rtn = []
-        deltas = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
-        for delta in deltas:
-            new_x = x + delta[0]
-            new_y = y + delta[1]
-            if game.square(new_x, new_y) in [FREE, 1 - colour]:
-                rtn.append(Move(game, x, y, new_x, new_y))
-        return rtn # cannot move into check? and through check. and castle.
-
-    def __repr__(self):
-        return 'K' if self.colour == WHITE else 'k'
+def king(game, x, y, colour):
+    rtn = []
+    deltas = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
+    for delta in deltas:
+        new_x = x + delta[0]
+        new_y = y + delta[1]
+        if game.square(new_x, new_y) in [FREE, 1 - colour]:
+            rtn.append(Move(game, x, y, new_x, new_y))
+    return rtn # cannot move into check? and through check. and castle.
 
 class Move():
     def __init__(self, game, old_x, old_y, new_x, new_y):
@@ -368,10 +320,14 @@ class Move():
         return '%s%d%s%d' % (chr(self.old_x + 97), self.old_y + 1,
                              chr(self.new_x + 97), self.new_y + 1)
 
-    def to_SAN(self): # incomplete
-        piece_letter = repr(self.piece).upper()
-        rtn = '' if piece_letter == 'P' else piece_letter
+    def to_SAN(self): # incomplete "short algebraic notation"
+        piece_letter = self.game.board[self.old_x][self.old_y].upper()
+        rtn = ''
+        if piece_letter != 'P':
+            rtn += piece_letter
         if self.game.board[self.new_x][self.new_y] != FREE:
+            if piece_letter == 'P':
+                rtn += chr(self.old_x + 97)
             rtn += 'x'
         rtn += self.to_string()[2:]
         return rtn
@@ -418,7 +374,6 @@ def play(pgn_output_file=None):
         else:
             agent = agent2
         print(game)
-        print(game.to_FEN())
         print('Time: %s, Eval: %.2f' % (time.time()-start, agent.static_eval(game)))
         move = agent.move(game)
 
@@ -461,6 +416,48 @@ class Shell(cmd.Cmd):
         sys.exit()
 
 if __name__ == '__main__':
-    play()
-    #play(pgn_output_file='replays/replay002.pgn') # terminal interface
+    #play()
+    play(pgn_output_file='replays/replay004.pgn') # terminal interface
     #Shell().cmdloop() # uci interface
+
+def test(): # sample game
+    g = Game()
+    Move(g, 4, 1, 4, 2).execute()
+    Move(g, 3, 6, 3, 4).execute()
+    Move(g, 3, 0, 7, 4).execute()
+    Move(g, 3, 7, 3, 5).execute()
+    Move(g, 5, 0, 1, 4).execute()
+    Move(g, 2, 6, 2, 5).execute()
+    Move(g, 1, 4, 4, 1).execute()
+    Move(g, 6, 7, 5, 5).execute()
+    Move(g, 7, 4, 5, 2).execute()
+    Move(g, 2, 7, 6, 3).execute()
+    Move(g, 5, 2, 6, 2).execute()
+    Move(g, 6, 3, 4, 1).execute()
+    Move(g, 6, 0, 4, 1).execute()
+    Move(g, 4, 6, 4, 5).execute()
+    Move(g, 6, 2, 5, 2).execute()
+    Move(g, 3, 5, 4, 4).execute()
+    Move(g, 3, 1, 3, 3).execute()
+    Move(g, 4, 4, 6, 4).execute()
+    Move(g, 4, 2, 4, 3).execute()
+    Move(g, 3, 4, 4, 3).execute()
+    Move(g, 2, 0, 6, 4).execute()
+    Move(g, 4, 3, 5, 2).execute()
+    Move(g, 4, 1, 5, 3).execute()
+    Move(g, 5, 5, 4, 3).execute()
+    Move(g, 7, 1, 7, 3).execute()
+    Move(g, 5, 2, 6, 1).execute()
+    Move(g, 7, 0, 7, 2).execute()
+    Move(g, 5, 7, 1, 3).execute()
+    Move(g, 1, 0, 3, 1).execute()
+    Move(g, 4, 3, 6, 4).execute()
+    Move(g, 7, 3, 6, 4).execute()
+    Move(g, 4, 5, 4, 4).execute()
+    Move(g, 3, 3, 4, 4).execute()
+    Move(g, 1, 7, 3, 6).execute()
+    Move(g, 0, 1, 0, 2).execute()
+    Move(g, 1, 3, 0, 4).execute()
+    Move(g, 1, 1, 1, 3).execute()
+    Move(g, 0, 4, 2, 6).execute()
+    return g
